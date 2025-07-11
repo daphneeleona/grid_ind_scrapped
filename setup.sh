8cd60d18acdf80b8f1ef48dcf43dfaac39a8ceea
daphneeleona <daphneeleona@gmail.com>
2025-07-11 20:49:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-GECKO_VERSION="v0.34.0"
-wget "https://github.com/mozilla/geckodriver/releases/download/${GECKO_VERSION}/geckodriver-${GECKO_VERSION}-linux64.tar.gz"
-tar -xvzf "geckodriver-${GECKO_VERSION}-linux64.tar.gz"
-chmod +x geckodriver
-mv geckodriver /usr/bin/
+CHROME_VERSION="120.0.6099.224"
+CHROMEDRIVER_VERSION="120.0.6099.224"
+
+# Download and install ChromeDriver
+wget "https://storage.googleapis.com/chrome-for-testing-public/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip"
+unzip chromedriver-linux64.zip
+chmod +x chromedriver-linux64/chromedriver
+mv chromedriver-linux64/chromedriver /usr/bin/chromedriver
