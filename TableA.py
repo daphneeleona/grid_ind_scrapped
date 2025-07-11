3e0b0b9048015aa3fa275f742e69a43ac206ae3d
daphneeleona <daphneeleona@gmail.com>
2025-07-11 11:16:14 +0530

    Update TableA.py

--- TableA.py
+++ TableA.py
@@ -23,14 +23,14 @@ def get_website_content(url):
         options.add_argument('--headless=new')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1200')
+        options.add_argument('--window-size=1920,1080')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
         
-        options.binary_location = "/usr/bin/chromium"
+        #options.binary_location = "/usr/bin/chromium"
         service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=options)
         driver.get(url)
