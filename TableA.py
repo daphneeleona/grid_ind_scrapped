a03838a16ed7d5ea1c5899789b81337433db6a9a
daphneeleona <daphneeleona@gmail.com>
2025-07-11 20:05:09 +0530

    Update TableA.py

--- TableA.py
+++ TableA.py
@@ -6,12 +6,13 @@ from io import BytesIO
 from datetime import datetime
 import urllib3
 
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -19,18 +20,15 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url = "https://grid-india.in/en/reports/daily-psp-report"
 def get_website_content(url):
     try:
-        options = webdriver.ChromeOptions()
+        options = FirefoxOptions()
         options.add_argument('--headless=new')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
         options.add_argument('--window-size=1920,1080')
         options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option('useAutomationExtension', False)
-        service = Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=service, options=options)
+        service = FirefoxService(executable_path="/usr/bin/geckodriver")
+        driver = webdriver.Firefox(service=service, options=options)
+
         driver.get(url)
         return driver
     except Exception as e:
