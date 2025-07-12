e9f8026c60903841ee2ade603e0e77c95734e5f2
daphneeleona <daphneeleona@gmail.com>
2025-07-12 10:39:19 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CHROME_VERSION="120.0.6099.224"
-CHROMEDRIVER_VERSION="120.0.6099.224"
-
-# Download and install ChromeDriver
-wget "https://storage.googleapis.com/chrome-for-testing-public/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip"
-unzip chromedriver-linux64.zip
-chmod +x chromedriver-linux64/chromedriver
-mv chromedriver-linux64/chromedriver /usr/bin/chromedriver
